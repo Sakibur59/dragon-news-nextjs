3ff458a868ebe299539632da52cc4c74c33ba874
Sakibur59 <sakiburrahman5978@gmail.com>
2026-04-25 00:16:24 +0600

    page not-found, google font added for diffrent pages, date fns added

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -1,12 +1,13 @@
 import Navbar from '@/Component/shared/Navbar';
 import React from 'react';
+import { montserrat } from '../layout';
 
 const AuthLayout = ({children}) => {
     return (
-        <>
+        <div className={`${montserrat.className}`}>
         <Navbar></Navbar>
         {children}
-        </>
+        </div>
     );
 };
 export default AuthLayout;
