e8eca12ae3f812b705ce4352898bed14655c256e
Sakibur59 <sakiburrahman5978@gmail.com>
2026-04-25 16:56:01 +0600

    display news card details

---
+++ src/Component/homepage/NewsCard.jsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { CiBookmark, CiShare2, CiStar } from "react-icons/ci";
+import { FaEye } from "react-icons/fa";
+import { IoIosStar } from "react-icons/io";
+
+const NewsCard = ({ news }) => {
+  return (
+   <div className="card bg-base-100  shadow-sm">
+  <div className="card-body">
+
+    <div className="flex justify-between items-center bg-slate-200 p-4">
+        <div className="flex gap-1 items-center">
+            <Image src={news.author?.img} alt={news.author?.name} height={40} width={40} className="rounded-full"></Image>
+        <div>
+            <h2 className="font-semibold">{news.author?.name}</h2>
+            <p className="text-xs">{news.author?.published_date}</p>
+        </div>
+        </div>
+        <div className="flex justify-between items-center">
+            <CiShare2></CiShare2>
+            <CiBookmark></CiBookmark>
+        </div>
+    </div>
+    <h2 className="card-title">{news.title}</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+     <figure>
+    <Image
+      src={news.image_url}
+      alt={news.title} width={300}
+      height={300}  className="w-full"/>
+  </figure>
+  <p className="line-clamp-3">{news.details}</p>
+  <div className="flex items-center gap-2 justify-between">
+    <div className="flex items-center gap-2">
+        <h2  className="flex items-center gap-2"><IoIosStar className="text-yellow-500 text-lg"></IoIosStar>
+            {news.rating.number}</h2>
+        <h2  className="flex items-center gap-2"><FaEye className="text-lg"></FaEye>{news.total_view}</h2>
+    </div>
+    <Link href={`/news/${news._id}`}>
+    <button className="btn btn-primary">See Details</button>
+    </Link>
+  </div>
+  </div>
+ 
+</div>
+  );
+};
+
+export default NewsCard;
--- src/app/(main)/category/[id]/page.jsx
+++ src/app/(main)/category/[id]/page.jsx
@@ -1,21 +1,10 @@
 import LeftSideBar from "@/Component/homepage/LeftSideBar";
+import NewsCard from "@/Component/homepage/NewsCard";
 import RightSideBar from "@/Component/homepage/RightSideBar";
+import { getCategories, getNewsByCategoryId } from "@/lib/data";
 import React from "react";
 
-async function getCategories() {
-  const res = await fetch(
-    "https://openapi.programming-hero.com/api/news/categories",
-  );
-  const data = await res.json();
-  return data.data;
-}
-async function getNewsByCategoryId(category_id) {
-  const res = await fetch(
-    `https://openapi.programming-hero.com/api/news/category/${category_id}`,
-  );
-  const data = await res.json();
-  return data.data;
-}
+
 
 const NewsCategoryPage = async ({ params }) => {
   const { id } = await params;
@@ -28,11 +17,11 @@ const NewsCategoryPage = async ({ params }) => {
       <div className="col-span-3  p-2">
         <LeftSideBar categories={categories} activeId={id}></LeftSideBar>
       </div>
-      <div className="font-bold col-span-6 p-2">
-        All News
-        <div className="space-y-4">
+      <div className="col-span-6 p-2">
+        <h2 className="font-bold text-lg">News By Category</h2>
+        <div className="space-y-4 mt-6">
           {news.length>0 ? news.map((n) => {
-            return <div key={n._id}>{n.title}</div>;
+            return <NewsCard key={n._id} news={n}></NewsCard>;
           }):<h2 className="font-bold text-5xl text-center my-7">No News Found!</h2>}
         </div>
       </div>
