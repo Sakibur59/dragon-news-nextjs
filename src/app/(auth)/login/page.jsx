0903ab44f660ee18aac812adc661586582d47fe9
Sakibur59 <sakiburrahman5978@gmail.com>
2026-04-26 15:03:56 +0600

    Login page are added

---
+++ src/app/(auth)/login/page.jsx
@@ -0,0 +1,55 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const LoginPage = () => {
+  const { register, handleSubmit,formState: { errors } } = useForm();
+  const handleLoginFunc = (data) => {
+    console.log(data)
+  };
+  return (
+    <div className="w-300 mx-auto min-h-[80vh] flex justify-center items-center bg-slate-100">
+      <div className="px-20 py-20 rounded-xl bg-white">
+        <h2 className="font-bold text-3xl text-center mb-6 ">
+          Login Your Account
+        </h2>
+        <form
+          onSubmit={handleSubmit(handleLoginFunc)}
+          className="space-y-4 border-t border-t-slate-200"
+        >
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Email Address</legend>
+            <input
+              {...register("email",{ required: "Email field is required"})}
+              type="email"
+              className="input"
+              placeholder="Type your Email"
+            />
+            {errors.email &&  <p className="text-red-500">{errors.email.message}</p>}
+          </fieldset>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Password</legend>
+            <input
+              {...register("password",{ required: "Password field is required"})}
+              type="password"
+              className="input"
+              placeholder="Enter your Password"
+            />
+            {errors.password &&  <p className="text-red-500">{errors.password.message}</p>}
+          </fieldset>
+          <button className="btn bg-slate-800 text-white w-full">Login</button>
+        </form>
+        <p className="mt-4 ">
+          Don't Have an account?
+          <Link href={"/register"} className="text-red-500">
+            {" "}
+            Register
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
