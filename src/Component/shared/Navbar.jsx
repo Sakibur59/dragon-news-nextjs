b43549ccb4aea747ec9e1eb48ad447c2a0970cd4
Sakibur59 <sakiburrahman5978@gmail.com>
2026-04-27 20:25:48 +0600

    show user name,photo and also signout user added

--- src/Component/shared/Navbar.jsx
+++ src/Component/shared/Navbar.jsx
@@ -1,14 +1,18 @@
-import React, { use } from "react";
-
+"use client";
+import React from "react";
 import userAvatar from "@/assets/user.png";
 import Image from "next/image";
 import Link from "next/link";
 import NavLink from "./NavLink";
+import { authClient } from "@/lib/auth-client";
 
 const Navbar = () => {
+  const { data: session, isPending } = authClient.useSession();
+  const user = session?.user;
+  console.log(user);
   return (
     <div className="w-300 mx-auto flex justify-between  mt-6">
-        <div></div>
+      <div></div>
       <ul className="flex justify-between items-center gap-4 text-gray-700">
         <li>
           <NavLink href={"/"}>Home</NavLink>
@@ -20,17 +24,24 @@ const Navbar = () => {
           <NavLink href={"/career"}>Career</NavLink>
         </li>
       </ul>
-      <div className="flex items-center gap-2">
-        <Image
-          src={userAvatar}
-          alt="user avatar"
-          width={60}
-          height={60}
-        ></Image>
+      {isPending ? (
+        <span className="loading loading-spinner loading-xl"></span>
+      ) : user ? (
+        <div className="flex items-center gap-2">
+          <h2>Hello, {user.name}</h2>
+          <Image
+            src={user.image || userAvatar}
+            alt="user avatar"
+            width={60}
+            height={60}
+          ></Image>
+          <button className="btn bg-purple-500 text-white" onClick={async ()=>await authClient.signOut()}>Logout</button>
+        </div>
+      ) : (
         <button className="btn bg-purple-500 text-white">
           <Link href={"/login"}>Login</Link>
         </button>
-      </div>
+      )}
     </div>
   );
 };
